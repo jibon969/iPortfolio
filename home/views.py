ff04e8367e0745cc0ec996917064703e82fcb2ec
jayed969 <jayed.swe@gmail.com>
2023-06-12 22:59:08 +0600

    added about me

--- home/views.py
+++ home/views.py
@@ -1,23 +1,10 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect, HttpResponse
-from .forms import ContactForm
+from django.shortcuts import render
 from django.contrib import messages
 
 
 def home(request):
-    if request.method == "POST":
-        form = ContactForm(request.POST or None)
-        if form.is_valid():
-            form.save()
-            messages.add_message(request, messages.SUCCESS, "Success! Thank you for your message.")
-            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    return render(request, "home/index.html")
 
-        else:
-            messages.add_message(request, messages.WARNING, "Please try again !")
-            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-    else:
-        form = ContactForm()
-    context = {
-        'from': form,
-    }
-    return render(request, "home/index.html", context)
+
+def about_me(request):
+    return render(request, 'home/about-me.html')
