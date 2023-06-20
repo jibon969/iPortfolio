868352ffe154386693939b5ef8bd84bbe2b6cfbd
jayed969 <jayed.swe@gmail.com>
2023-06-20 23:27:43 +0600

    added login view

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.shortcuts import render
 from .forms import RegisterForm, LoginForm
-
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views
 def register_view(request):
@@ -15,3 +15,22 @@ def register_view(request):
     else:
         form = RegisterForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=email, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    messages.info(request, f"You are now logged in as {email}")
+                return redirect('home')
+            else:
+                messages.add_message(request, messages.WARNING, "Invalid email or password.")
+    else:
+        form = LoginForm()
+    return render(request, "accounts/login.html", {"form": form})
