c6a2a46ff1882775a252e8fec2c74ec845343231
jibontech <jibon.belaface@gmail.com>
2023-06-22 13:53:14 +0600

    added password reset done

--- accounts/urls.py
+++ accounts/urls.py
@@ -24,9 +24,10 @@ urlpatterns = [
         template_name='accounts/register/password_reset.html',
         email_template_name='accounts/register/password_reset_email.html',
         subject_template_name='accounts/register/password_reset_subject.txt'), name='password_reset'),
+
+    path('reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/register/password_reset_done.html'), name='password_reset_done'),
         
-    # path('reset/done/', auth_views.PasswordResetDoneView.as_view(
-    #     template_name='accounts/register/password_reset_done.html'), name='password_reset_done'),
     # path('register-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
     #     template_name='accounts/register/password_reset_confirm.html'), name='password_reset_confirm'),
     # path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(
