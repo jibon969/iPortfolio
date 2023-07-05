7f443ffb3afeef1ad3588976c68b13f81ad80dc0
jayed969 <jayed.swe@gmail.com>
2023-07-05 21:09:42 +0600

    added notification html

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Notification
+from django.contrib import messages
 
-# Create your views here.
+
+def notification_list(request):
+    if request.user.is_authenticated and request.user.is_staff:
+        notifications = Notification.objects.filter(user=request.user).order_by('-id')
+        return render(request, 'notification/notification.html', {'notifications': notifications})
+    else:
+        messages.add_message(request, messages.WARNING, "Sorry currently you don't have permission to access this file")
+        return redirect('home')
+
+
+
